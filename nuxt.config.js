193d1f4e706dfc5f04561ced8cf9b71a9b576dd4
小倉裕一 <j211025ogu@gmail.com>
2018-07-02 19:06:13 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'RPG',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
+      { hid: 'description', name: 'description', content: 'rpg-sample' }
+    ],
+    link: [
+    ]
+  },
+  plugins: [
+  ],
+  css: [
+    '~assets/common.css'
+  ]
+}
